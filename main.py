c8b28b3a0afa1c9f940430ee6a0ef00ff7e36e9c
Jakob <jakob.m@dsen.tv>
2021-09-08 19:49:07 +0200

    logging in main.py

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ def main():
 
             output += f'{folder}/{f}|{title}'
 
+    print(len(files), 'files found')
+
     #files = [f for f in os.listdir(folder) if '.md' in f]
     #output = '\n'.join([ f'<li href=\"./posts/{filename}\">{filename.split(".")[0]}</li>' for filename in files ])
 
     with open('posts.txt', 'w') as f:
         f.write(output)
+
+    print('created file')
+    print([f for f in os.listdir() if '.txt' in f])
 
 if __name__ == '__main__':
     main()
