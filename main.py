8f80eb01a520dde23c8a2624847ac2fe6398002c
Jakob <jakob.m@dsen.tv>
2021-09-08 19:45:25 +0200

    test

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
             title = content[0]
             title = title[3:-3].strip()
 
-            output += f'<li href="{folder}/{f}">{title}</li>'
+            output += f'{folder}/{f}|{title}'
 
     #files = [f for f in os.listdir(folder) if '.md' in f]
     #output = '\n'.join([ f'<li href=\"./posts/{filename}\">{filename.split(".")[0]}</li>' for filename in files ])
